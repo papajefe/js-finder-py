899a57ba55b912e174da6d1a708b2c108902e604
lincoln-lm <73306575+Lincoln-LM@users.noreply.github.com>
2023-02-17 13:22:21 -0700

    restructure + basic seed-to-time program

--- build.sh
+++ build.sh
@@ -13,7 +13,9 @@ cd ..
 
 # Copy to build folder
 mkdir build
+mkdir ./build/wheels
 cp -r ./css/ ./build/css/
+cp -r ./pages/ ./build/pages/
 cp ./index.html ./build/
-cp ./numba_pokemon_prngs-0.1.0-py3-none-any.whl ./build/
-cp ./js_finder/dist/js_finder-0.1.0-py3-none-any.whl ./build
+cp ./numba_pokemon_prngs-0.1.0-py3-none-any.whl ./build/wheels/
+cp ./js_finder/dist/js_finder-0.1.0-py3-none-any.whl ./build/wheels/
--- js_finder/js_finder/seed_to_time.py
+++ js_finder/js_finder/seed_to_time.py
@@ -1,11 +1,36 @@
 """Seed to Time module for pyodide to access"""
 import sys
-import numba_pokemon_prngs
+import numpy as np
+from numba_pokemon_prngs import mersenne_twister
 
 
 def main():
     """Main function to be run for the seed_to_time module"""
-    lcrng = numba_pokemon_prngs.lcrng.PokeRNGDiv(0x12345678)
+    np.seterr(over="ignore", under="ignore")
     print("Hello from seed to time!")
-    print(f"{lcrng.next()=:08X}")
     print(f"{sys.version=}")
+
+
+def test_coinflip(target_seed: np.uint32, coinflip_seq: str):
+    """Test the coinflips for a target seed"""
+    target_seed = np.uint32(target_seed)
+    coinflip_seq = tuple(int(flip) for flip in coinflip_seq)
+
+    seed_high = target_seed >> np.uint32(24)
+    seed_hour = (target_seed >> np.uint32(16)) & np.uint32(0xFF)
+    seed_delay = target_seed & np.uint32(0xFFFF)
+
+    test_seed_hour = np.uint32(seed_hour)
+    for second in range(-2, 3):
+        test_seed_high = (seed_high + np.uint32(second)) & np.uint32(0xFF)
+        for delay in range(-200, 201):
+            test_seed_delay = (seed_delay + np.uint32(delay)) & np.uint32(0xFFFFFFFF)
+            test_seed = (
+                (test_seed_high << np.uint32(24))
+                | (test_seed_hour << np.uint32(16))
+                | test_seed_delay
+            )
+            rng = mersenne_twister.MersenneTwister(test_seed)
+            if all((rng.next() & 1) == flip for flip in coinflip_seq):
+                print(f"{test_seed=:08X} is valid!")
+    print("Search Done!")

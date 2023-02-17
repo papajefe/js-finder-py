d01eaf2e7fadbd324a1f2f1bb39556c65c5b3822
lincoln-lm <73306575+Lincoln-LM@users.noreply.github.com>
2023-02-17 12:00:53 -0700

    css

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ cd ..
 
 # Copy to build folder
 mkdir build
+cp -r ./css/ ./build/css/
 cp ./index.html ./build/
 cp ./numba_pokemon_prngs-0.1.0-py3-none-any.whl ./build/
 cp ./js_finder/dist/js_finder-0.1.0-py3-none-any.whl ./build

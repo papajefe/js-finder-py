bfcb4aa120ec93c5eea969834eca5e1fdcfe1437
lincoln-lm <73306575+Lincoln-LM@users.noreply.github.com>
2023-02-22 01:04:22 -0700

    download numba-pokemon-prngs on pages build

--- build.sh
+++ build.sh
@@ -6,16 +6,20 @@ rm -r build
 # install build requirements
 pip install -r requirements.txt
 
-# Build modules
+# build modules
 cd ./js_finder/
 poetry run poetry build --format=wheel
 cd ..
 
-# Copy to build folder
+# download modules
+mkdir dist
+wget https://github.com/Lincoln-LM/numba_pokemon_prngs/releases/download/latest/numba_pokemon_prngs-0.1.0-py3-none-any.whl -O ./dist/numba_pokemon_prngs-0.1.0-py3-none-any.whl
+
+# copy to build folder
 mkdir build
 mkdir ./build/wheels
 cp -r ./css/ ./build/css/
 cp -r ./pages/ ./build/pages/
 cp ./index.html ./build/
-cp ./numba_pokemon_prngs-0.1.0-py3-none-any.whl ./build/wheels/
+cp ./dist/numba_pokemon_prngs-0.1.0-py3-none-any.whl ./build/wheels/
 cp ./js_finder/dist/js_finder-0.1.0-py3-none-any.whl ./build/wheels/

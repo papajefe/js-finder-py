5e6466f3bd518ebf5465e89085dc3580562f0df5
Lincoln-LM <73306575+Lincoln-LM@users.noreply.github.com>
2025-01-27 13:38:45 -0700

    implement iv calculator

--- js_finder/js_finder/g3_calibration.py
+++ js_finder/js_finder/g3_calibration.py
@@ -5,7 +5,8 @@ import sys
 from typing import Iterable
 import numpy as np
 from numba_pokemon_prngs.lcrng import PokeRNGMod
-from numba_pokemon_prngs.data import NATURES_EN
+from numba_pokemon_prngs.data import NATURES_EN, SPECIES_EN
+from numba_pokemon_prngs.data.personal import PERSONAL_INFO_FR
 from .ten_lines import FRLG_DATA
 
 
@@ -14,6 +15,75 @@ def main():
     np.seterr(over="ignore", under="ignore")
     print("Hello from g3 calibration!")
     print(f"{sys.version=}")
+
+
+def fetch_species_options():
+    return "".join(f'<option value="{i}">{species}</option>' for i,species in enumerate(SPECIES_EN[1:387], 1))
+
+def try_intersect(x, y):
+    try:
+        return range(max(x[0], y[0]), min(x[-1], y[-1]) + 1)
+    except IndexError:
+        return range(32, 0)
+
+def calc_stat(stat_index, base_stat, iv, level, nature):
+    iv_map = (-1, 0, 1, 3, 4, 2)
+    stat = np.uint16(
+        np.uint16((np.uint16(2) * base_stat + iv) * level) // np.uint16(100)
+    )
+    nature_boost = nature // np.uint8(5)
+    nature_decrease = nature % np.uint8(5)
+    if stat_index == 0:
+        stat += np.uint16(level) + np.uint16(10)
+    else:
+        stat += np.uint16(5)
+        if nature_boost != nature_decrease:
+            if iv_map[stat_index] == nature_boost:
+                stat = np.uint16(stat * np.float32(1.1))
+            elif iv_map[stat_index] == nature_decrease:
+                stat = np.uint16(stat * np.float32(0.9))
+    return stat
+
+def calc_ivs(base_stats, stats, level, nature):
+    min_ivs = np.ones(6, np.uint16) * 31
+    max_ivs = np.zeros(6, np.uint16)
+    for i in range(6):
+        for iv in range(32):
+            stat = calc_stat(i, base_stats[i], np.uint8(iv), np.uint8(level), np.uint8(nature))
+            if stat == stats[i]:
+                min_ivs[i] = min(iv, min_ivs[i])
+                max_ivs[i] = max(iv, max_ivs[i])
+    return tuple(
+        range(min_iv, max_iv + 1) for min_iv, max_iv in zip(min_ivs, max_ivs)
+    )
+
+def calculate_ivs(species: int, nature: int, stat_entry: str):
+    personal_info = PERSONAL_INFO_FR[species]
+    base_stats = np.array(
+        (
+            personal_info.hp,
+            personal_info.attack,
+            personal_info.defense,
+            personal_info.special_attack,
+            personal_info.special_defense,
+            personal_info.speed,
+        ),
+        np.uint16,
+    )
+    iv_ranges = [range(32) for _ in range(6)]
+    for line in stat_entry.split("\n"):
+        level, hp, atk, def_, spa, spd, spe = map(int, line.split())
+        stats = np.array((hp, atk, def_, spa, spd, spe), np.uint16)
+        iv_ranges = [
+            try_intersect(x, y)
+            for x, y in zip(
+                iv_ranges,
+                calc_ivs(
+                    base_stats, stats, level, np.int8(nature)
+                ),
+            )
+        ]
+    return iv_ranges
 
 def check_frlg(
     method: int,
